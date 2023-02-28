8c943612c4d63bca1705e12bd69c504ba88c0b29
Souheil007 <107995350+Souheil007@users.noreply.github.com>
2023-02-28 23:04:03 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import InputForm from './components/InputForm'
+
+
+function App() {
+
+  
+  return (
+    <div className="App">
+     
+      <InputForm></InputForm>
+      
+     
+    </div>
+  )
+}
+
+export default App
